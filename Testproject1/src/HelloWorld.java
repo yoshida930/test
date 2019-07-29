c8713808ecd6935202765d05db52328dbb569d24
dashimakin <dashimakin@gmail.com>
2019-07-29 21:21:25 +0900

    19/07/29

---
+++ Testproject1/src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		String[]name=new String[3];
+		
+		name[0]="田中";
+		name[1]="高橋";
+		name[2]="斉藤";
+		System.out.println(name[0]);
+		}
+	 }
+ 
+
